d33f9794c4399af250051024aec4a39d60c3ddbf
Favas Muhammed <favassakkeer13@gmail.com>
2024-08-29 09:37:26 +0200

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,35 @@
 // Iteration 1: Names and Input
+const hacker1= "Favas";
+const hacker2= "Eduard";
 
+console.log(`The driver's name is ${hacker1}`)
+console.log(`The navigator's name is ${hacker2}`)
 
 // Iteration 2: Conditionals
+const lengthHacker1 = hacker1.length;
+const lengthHacker2 = hacker2.length;
 
-
+if(lengthHacker1>lengthHacker2){
+    console.log(`The driver has the longest name, it has ${lengthHacker1} characters.`)
+    
+     }
+    if(lengthHacker1<lengthHacker2){
+        console.log(` It seems that the navigator has the longest name, it has ${lengthHacker2} characters.`)
+    
+    }else{
+    
+    console.log("What?! You both have the same name?")
+    }
+    
 // Iteration 3: Loops
+let result1 = "";
+for(let count1 = 0; count1<hacker1.length; count1++){
+    result1 +=hacker1[count1]+" ";
+}
+  console.log(result1)
+
+  let result2 = "";
+  for(let lastLetter = hacker2.length-1;0<= lastLetter; lastLetter--){
+    result2 += hacker2[lastLetter]
+}
+console.log(result2)
